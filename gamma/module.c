f1489cd6e418f3f2cc0a9b4a547c74babfc9c778
Calin Reamy <nilac16@ymail.com>
2025-08-31 13:29:51 -0400

    Use NumPy constructor instead of PyMem_Malloc

--- gamma/module.c
+++ gamma/module.c
@@ -6,6 +6,18 @@
 #include "gamma.h"
 
 
+struct gpy_distribution {
+    struct gamma_distribution dist; /* Base distribution object used by C */
+    PyArrayObject            *data; /* Borrowed data reference */
+};
+
+
+struct gpy_results {
+    struct gamma_results res;   /* The results buffer used by the C code */
+    PyArrayObject       *arr;   /* NumPy array containing the gamma distrib. */
+};
+
+
 static bool gpy_get_long(PyObject *obj, const char *attr, long *res)
 {
     PyObject *ptr;
@@ -69,7 +81,7 @@ static bool gpy_get_arrayf(PyObject *obj, const char *attr,
     }
     if (PyArray_TYPE((PyArrayObject *)ptr) != NPY_DOUBLE) {
         PyErr_SetString(PyExc_TypeError,
-                        "All NumPy array must have dtype=double");
+                        "All NumPy arrays must have dtype=double");
         return false;
     }
     if ((size_t)PyArray_SIZE((PyArrayObject *)ptr) < len) {
@@ -136,68 +148,66 @@ static bool gpy_load_options(struct gamma_options *opts, PyObject *obj)
 }
 
 
-static bool gpy_load_distribution(struct gamma_distribution *dist,
-                                  PyObject                  *obj)
+static bool gpy_load_distribution(struct gpy_distribution *dist, PyObject *obj)
 {
-    PyObject *data;
     double buffer[9];
     npy_intp *dims;
 
     if (!gpy_get_arrayf(obj, "matrix", 9, buffer)) {
         return false;
     }
-    dist->matrix.cols[0].vec[0] = buffer[0];
-    dist->matrix.cols[0].vec[1] = buffer[1];
-    dist->matrix.cols[0].vec[2] = buffer[2];
-    dist->matrix.cols[0].vec[3] = 0.0;
+    dist->dist.matrix.cols[0].vec[0] = buffer[0];
+    dist->dist.matrix.cols[0].vec[1] = buffer[1];
+    dist->dist.matrix.cols[0].vec[2] = buffer[2];
+    dist->dist.matrix.cols[0].vec[3] = 0.0;
 
-    dist->matrix.cols[1].vec[0] = buffer[3];
-    dist->matrix.cols[1].vec[1] = buffer[4];
-    dist->matrix.cols[1].vec[2] = buffer[5];
-    dist->matrix.cols[1].vec[3] = 0.0;
+    dist->dist.matrix.cols[1].vec[0] = buffer[3];
+    dist->dist.matrix.cols[1].vec[1] = buffer[4];
+    dist->dist.matrix.cols[1].vec[2] = buffer[5];
+    dist->dist.matrix.cols[1].vec[3] = 0.0;
 
-    dist->matrix.cols[2].vec[0] = buffer[6];
-    dist->matrix.cols[2].vec[1] = buffer[7];
-    dist->matrix.cols[2].vec[2] = buffer[8];
-    dist->matrix.cols[2].vec[3] = 0.0;
+    dist->dist.matrix.cols[2].vec[0] = buffer[6];
+    dist->dist.matrix.cols[2].vec[1] = buffer[7];
+    dist->dist.matrix.cols[2].vec[2] = buffer[8];
+    dist->dist.matrix.cols[2].vec[3] = 0.0;
 
     if (!gpy_get_arrayf(obj, "origin", 3, buffer)) {
         return false;
     }
-    dist->matrix.cols[3].vec[0] = buffer[0];
-    dist->matrix.cols[3].vec[1] = buffer[1];
-    dist->matrix.cols[3].vec[2] = buffer[2];
-    dist->matrix.cols[3].vec[3] = 1.0;
+    dist->dist.matrix.cols[3].vec[0] = buffer[0];
+    dist->dist.matrix.cols[3].vec[1] = buffer[1];
+    dist->dist.matrix.cols[3].vec[2] = buffer[2];
+    dist->dist.matrix.cols[3].vec[3] = 1.0;
 
     if (!gpy_get_arrayf(obj, "spacing", 3, buffer)) {
         return false;
     }
-    dist->matrix.cols[0] = gamma_vec_muls(&dist->matrix.cols[0], buffer[0]);
-    dist->matrix.cols[1] = gamma_vec_muls(&dist->matrix.cols[1], buffer[1]);
-    dist->matrix.cols[2] = gamma_vec_muls(&dist->matrix.cols[2], buffer[2]);
+    dist->dist.matrix.cols[0] = gamma_vec_muls(&dist->dist.matrix.cols[0], buffer[0]);
+    dist->dist.matrix.cols[1] = gamma_vec_muls(&dist->dist.matrix.cols[1], buffer[1]);
+    dist->dist.matrix.cols[2] = gamma_vec_muls(&dist->dist.matrix.cols[2], buffer[2]);
 
-    data = PyObject_GetAttrString(obj, "data");
-    if (!data) {
+    dist->data = (PyArrayObject *)PyObject_GetAttrString(obj, "data");
+    if (!dist->data) {
         return false;
     }
-    Py_DECREF(data);
-    if (!PyArray_Check(data)) {
+    Py_DECREF(dist->data);
+    if (!PyArray_Check(dist->data)) {
         PyErr_SetString(PyExc_ValueError, "Dose data must be a NumPy array");
         return false;
     }
 
-    if (PyArray_TYPE((PyArrayObject *)data) != NPY_DOUBLE) {
+    if (PyArray_TYPE((PyArrayObject *)dist->data) != NPY_DOUBLE) {
         PyErr_SetString(PyExc_TypeError,
                         "Dose data array must have dtype=double");
         return false;
     }
-    dims = PyArray_DIMS((PyArrayObject *)data);
-    dist->dims.idx[0] = dims[0];
-    dist->dims.idx[1] = dims[1];
-    dist->dims.idx[2] = dims[2];
+    dims = PyArray_DIMS((PyArrayObject *)dist->data);
+    dist->dist.dims.idx[0] = dims[0];
+    dist->dist.dims.idx[1] = dims[1];
+    dist->dist.dims.idx[2] = dims[2];
 
-    if (!gamma_distribution_set(dist, &dist->matrix, &dist->dims,
-                                PyArray_DATA((PyArrayObject *)data))) {
+    if (!gamma_distribution_set(&dist->dist, &dist->dist.matrix, &dist->dist.dims,
+                                PyArray_DATA((PyArrayObject *)dist->data))) {
         PyErr_SetString(PyExc_ArithmeticError, "Affine matrix is singular");
         return false;
     }
@@ -236,44 +246,25 @@ static bool gpy_write_double(double x, PyObject *obj, const char *attr)
 }
 
 
-static bool gpy_steal_array(double           **ptr,
-                            const gamma_idx_t *dims,
-                            PyObject          *obj,
-                            const char        *attr)
+static bool gpy_write_array(PyArrayObject *arr, PyObject *obj, const char *attr)
 {
-    const npy_intp dimens[3] = { dims->idx[0], dims->idx[1], dims->idx[2] };
-    PyObject *node;
     bool res;
 
-    /*
-     * God I hope I did this right, imagine your C documentation being SO SPARSE
-     * of a topic like MEMORY MANAGEMENT
-     */
-    node = PyArray_SimpleNewFromData(BUFLEN(dimens), dimens, NPY_DOUBLE, *ptr);
-    if (!node) {
-        return false;
-    }
-    res = !PyObject_SetAttrString(obj, attr, node);
-    if (res) {
-        PyArray_ENABLEFLAGS((PyArrayObject *)node, NPY_ARRAY_OWNDATA);
-        *ptr = NULL;
-    }
-    Py_DECREF(node);
+    res = !PyObject_SetAttrString(obj, attr, (PyObject *)arr);
+    Py_DECREF(arr);
     return res;
 }
 
 
-static bool gpy_write_results(struct gamma_results *res,
-                              const gamma_idx_t    *dims,
-                              PyObject             *obj)
+static bool gpy_write_results(struct gpy_results *res, PyObject *obj)
 {
-    return gpy_write_long(res->stats.total, obj, "total")
-        && gpy_write_long(res->pass, obj, "passed")
-        && gpy_write_double(res->stats.min, obj, "min")
-        && gpy_write_double(res->stats.max, obj, "max")
-        && gpy_write_double(res->stats.mean, obj, "mean")
-        && gpy_write_double(res->stats.msqr, obj, "msqr")
-        && gpy_steal_array(&res->dist, dims, obj, "dist");
+    return gpy_write_long(res->res.stats.total, obj, "total")
+        && gpy_write_long(res->res.pass, obj, "passed")
+        && gpy_write_double(res->res.stats.min, obj, "min")
+        && gpy_write_double(res->res.stats.max, obj, "max")
+        && gpy_write_double(res->res.stats.mean, obj, "mean")
+        && gpy_write_double(res->res.stats.msqr, obj, "msqr")
+        && gpy_write_array(res->arr, obj, "dist");
 }
 
 
@@ -282,8 +273,8 @@ static PyObject *gpy_compute(PyObject *self, PyObject *args)
     PyObject *pyparms, *pyopts, *pyref, *pymeas, *pyres;
     struct gamma_params params;
     struct gamma_options opts;
-    struct gamma_distribution ref, meas;
-    struct gamma_results res;
+    struct gpy_distribution ref, meas;
+    struct gpy_results res;
     bool code;
 
     (void)self;
@@ -299,13 +290,15 @@ static PyObject *gpy_compute(PyObject *self, PyObject *args)
         return NULL;
     }
 
-    res.dist = PyMem_Malloc(sizeof *res.dist * meas.len);
-    if (!res.dist) {
+    res.arr = (PyArrayObject *)PyArray_NewLikeArray(meas.data, NPY_KEEPORDER, NULL, 1);
+    if (!res.arr) {
         return NULL;
     }
-    gamma_compute(&params, &opts, &ref, &meas, &res);
-    code = gpy_write_results(&res, &meas.dims, pyres);
-    PyMem_Free(res.dist);
+    res.res.dist = PyArray_DATA(res.arr);
+
+    gamma_compute(&params, &opts, &ref.dist, &meas.dist, &res.res);
+    code = gpy_write_results(&res, pyres);
+    Py_DECREF(res.res.dist);
 
     return code ? Py_None : NULL;
 }
